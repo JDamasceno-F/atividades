647d831ebebae54290b49d6811135671291b2469
Jose damasceno <jdamasceno2806@gmail.com>
2025-11-26 00:40:41 -0300

    Finalizando sistema de batalha e monstros

---
+++ AT019.py
@@ -0,0 +1,21 @@
+from AT017APOIO import Personagem
+
+class Monstro(Personagem):
+    
+    # Este é o método "Fábrica"
+    @classmethod
+    def criar_goblin(cls):
+        # Aqui definimos os atributos PADRÃO de todo Goblin
+        return cls("Goblin", 50, 10)
+
+    @classmethod
+    def criar_chefao(cls):
+        return cls("Rei Goblin", 200, 40)
+    
+monstro1 = Monstro.criar_goblin()
+monstro2 = Monstro.criar_goblin()
+monstro3 = Monstro.criar_goblin()
+chefao = Monstro.criar_chefao()
+
+print(f"Monstro 1: {monstro1.nome} | Vida: {monstro1.get_vida()}")
+print(f"Chefe: {chefao.nome} | Vida: {chefao.get_vida()}")
